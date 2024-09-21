acaa61efa60bb8431ddb647956bb96cac4ce6d30
pavannaiik <pavan.naiik1@gmail.com>
2024-09-20 20:24:55 -0400

    assignment 1

--- assignment1-1/q1.go
+++ assignment1-1/q1.go
@@ -2,14 +2,19 @@ package cos418_hw1_1
 
 import (
 	"fmt"
+	"os"
+	"regexp"
 	"sort"
+	"strings"
 )
 
 // Find the top K most common words in a text document.
-// 	path: location of the document
+//
+//	path: location of the document
 //	numWords: number of words to return (i.e. k)
 //	charThreshold: character threshold for whether a token qualifies as a word,
 //		e.g. charThreshold = 5 means "apple" is a word but "pear" is not.
+//
 // Matching is case insensitive, e.g. "Orange" and "orange" is considered the same word.
 // A word comprises alphanumeric characters only. All punctuations and other characters
 // are removed, e.g. "don't" becomes "dont".
@@ -18,7 +23,50 @@ func topWords(path string, numWords int, charThreshold int) []WordCount {
 	// TODO: implement me
 	// HINT: You may find the `strings.Fields` and `strings.ToLower` functions helpful
 	// HINT: To keep only alphanumeric characters, use the regex "[^0-9a-zA-Z]+"
-	return nil
+	// Read the file content
+	data, err := os.ReadFile(path)
+	//checkError(err)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+	text := string(data)
+	text = strings.ToLower(text)
+
+	reg, err := regexp.Compile("[^0-9a-zA-Z]+")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Split the cleaned text into words
+	words := strings.Fields(text)
+
+	// Map to store word counts
+	wordCounts := make(map[string]int)
+
+	// Count the words that meet the charThreshold
+	for _, word := range words {
+		cleanedWord := reg.ReplaceAllString(word, "")
+		if len(cleanedWord) >= charThreshold {
+			wordCounts[cleanedWord]++
+		}
+	}
+
+	// Convert the map to a slice of WordCount
+	var wordCountList []WordCount
+	for word, count := range wordCounts {
+		wordCountList = append(wordCountList, WordCount{Word: word, Count: count})
+	}
+
+	// Sort the word counts
+	sortWordCounts(wordCountList)
+
+	// Return the top numWords results
+	if numWords > len(wordCountList) {
+		numWords = len(wordCountList)
+	}
+	return wordCountList[:numWords]
 }
 
 // A struct that represents how many times a word is observed in a document
--- assignment1-1/q2.go
+++ assignment1-1/q2.go
@@ -3,33 +3,73 @@ package cos418_hw1_1
 import (
 	"bufio"
 	"io"
+	"os"
 	"strconv"
+	"sync"
 )
 
 // Sum numbers from channel `nums` and output sum to `out`.
 // You should only output to `out` once.
-// Do NOT modify function signature.
 func sumWorker(nums chan int, out chan int) {
-	// TODO: implement me
-	// HINT: use for loop over `nums`
+	sum := 0
+	for num := range nums {
+		sum += num
+	}
+	// Output the sum to the out channel
+	out <- sum
 }
 
 // Read integers from the file `fileName` and return sum of all values.
 // This function must launch `num` go routines running
 // `sumWorker` to find the sum of the values concurrently.
-// You should use `checkError` to handle potential errors.
-// Do NOT modify function signature.
 func sum(num int, fileName string) int {
-	// TODO: implement me
-	// HINT: use `readInts` and `sumWorkers`
-	// HINT: used buffered channels for splitting numbers between workers
-	return 0
+	// Open the file
+	file, err := os.Open(fileName)
+	checkError(err)
+	defer file.Close()
+
+	// Read all integers from the file
+	ints, err := readInts(file)
+	checkError(err)
+
+	// Create channels for workers
+	nums := make(chan int, len(ints)) // Buffered channel for integers
+	out := make(chan int, num)        // Buffered channel for sum from each worker
+	var wg sync.WaitGroup             // WaitGroup to wait for all workers to finish
+
+	// Launch workers
+	for i := 0; i < num; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			sumWorker(nums, out)
+		}()
+	}
+
+	// Feed the numbers into the nums channel
+	for _, num := range ints {
+		nums <- num
+	}
+	close(nums) // Close nums channel after all numbers have been sent
+
+	// Wait for workers to finish summing
+	go func() {
+		wg.Wait()
+		close(out) // Close the out channel after all workers are done
+	}()
+
+	// Collect the results from the out channel
+	totalSum := 0
+	for partialSum := range out {
+		totalSum += partialSum
+	}
+
+	return totalSum
 }
 
 // Read a list of integers separated by whitespace from `r`.
 // Return the integers successfully read with no error, or
 // an empty slice of integers and the error that occurred.
-// Do NOT modify this function.
 func readInts(r io.Reader) ([]int, error) {
 	scanner := bufio.NewScanner(r)
 	scanner.Split(bufio.ScanWords)
